8606805aba3bc76cdd6556a6c67baa54ed082e09
Roman <pifko22@gmail.com>
2016-02-16 23:49:16 +0200

    third commit. Add view "EditText" to activity_main.xml.

---
+++ app/src/main/java/com/pifko22/roman/myapplication/MyTwoClass.java
@@ -0,0 +1,15 @@
+package com.pifko22.roman.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Roman on 16.02.2016.
+ */
+public class MyTwoClass extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
